5ac143e90c87e54106aac7424765e99975b85730
Karbhari Gadekar <karbharigadekar706@gmail.com>
2023-08-30 16:13:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react'
+import { questions } from './questions';
+
 
 function App() {
+ 
+
+  let [showfinal, setShowfinal] = useState(false);
+  let [currentscore, setCurrentscore] = useState(0);
+  let [currentQuestion, setCurrentQuestion] = useState(0)
+ 
+  const optionClicked = (isCorrect) => {
+    if (isCorrect) {
+      setCurrentscore(currentscore + 1);
+
+    }
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1)
+    } else {
+      setShowfinal(true)
+    }
+
+  }
+
+  const restart = () => {
+    setCurrentQuestion(0);
+    setCurrentscore(0);
+    setShowfinal(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 style={{ marginTop: "20px", fontWeight: "bolder" }}>USA Quiz</h1>
+
+      <h3 style={{ marginBottom: "40px" }}>Current Score : {currentscore}</h3>
+      {
+        showfinal ? <div className="result">
+          <h1>Result</h1>
+          <h3 style={{ margin: "10px" }}>{currentscore} out of {questions.length} are  Correct - (&nbsp;{(currentscore / questions.length) * 100}%&nbsp;)</h3>
+          <button className="btn btn-danger" onClick={restart} >Restart</button>
+        </div> :
+          <div className="question-card">
+            <h2>Question {currentQuestion + 1} out of {questions.length}</h2>
+            <h3 className="question-text">{questions[currentQuestion].text} </h3>
+            <ul>
+              {
+                questions[currentQuestion].options.map((ele) => {
+                  return (
+                    <li onClick={() => { optionClicked(ele.isCorrect) }} key={ele.id} >{ele.text}</li>
+                  )
+                })
+              }
+            </ul>
+
+          </div>
+      }
+
+
+
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,47 @@
+export const questions = [
+    {
+      text: "What is the capital of America?",
+      options: [
+        { id: 0, text: "New York City", isCorrect: false },
+        { id: 1, text: "New Jersy", isCorrect: false },
+        { id: 2, text: "Santa Fe", isCorrect: false },
+        { id: 3, text: "Washington DC", isCorrect: true },
+      ],
+    },
+    {
+      text: "When Was Chandryan 3 launched ?",
+      options: [
+        { id: 0, text: "2018", isCorrect: false },
+        { id: 1, text: "2019", isCorrect: false },
+        { id: 2, text: "2012", isCorrect: false },
+        { id: 3, text: "2023", isCorrect: true },
+      ],
+    },
+    {
+      text: "Who is current PM of INDIA",
+      options: [
+        { id: 0, text: "Narendra Modi", isCorrect: true },
+        { id: 1, text: "Dr Manmohan Singh", isCorrect: false },
+        { id: 2, text: "Rahul Gandhi", isCorrect: false },
+        { id: 3, text: "Amit Shah", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest state in the INDIA",
+      options: [
+        { id: 0, text: "Maharashtra", isCorrect: false },
+        { id: 1, text: "Gujrat", isCorrect: false },
+        { id: 2, text: "Uttar Pradesh", isCorrect: true },
+        { id: 3, text: "Madhya Pradesh", isCorrect: false },
+      ],
+    },
+    {
+      text: "When was Bangladesh formed ?",
+      options: [
+        { id: 0, text: "1924", isCorrect: false },
+        { id: 1, text: "1971", isCorrect: true },
+        { id: 2, text: "1999", isCorrect: false },
+        { id: 3, text: "2000", isCorrect: false },
+      ],
+    },
+  ];
